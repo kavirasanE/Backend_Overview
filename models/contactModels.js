ad0ef2d7ce31e08f494b832f56495eaa193093da
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2023-12-03 17:16:45 +0530

    database creation

--- models/contactModels.js
+++ models/contactModels.js
@@ -1,5 +1,24 @@
 import mongoose from "mongoose";
 
-const ContactSchema = mongoose.model (
-    
-)
+const ContactSchema = mongoose.Schema ({
+  name :{
+    type:String,
+    required:[true ,"please add the name"],
+  },
+  email :{
+    type:String,
+    required:true
+  },
+  phone :{
+    typr:String,
+    required:true
+  }
+
+},
+{
+    timestamps:true,
+});
+
+const contact = mongoose.model ("contact",ContactSchema)
+
+export default contact 
