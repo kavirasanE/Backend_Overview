1ee8be29387ae6cb99bd36058fcec2678bc39dde
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2023-12-03 14:09:23 +0530

    server started

---
+++ controller/userController.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+const app =express();
+dotenv.config();
+const port =process.env.PORT;
+
+app.listen( port ,() => {
+   console.log(`servcer is stated at ${port}`)
+});
